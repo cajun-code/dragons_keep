dbe00add297b55bd55628d9bd8634b5be38894a5
Allan Davis <javaalley@gmail.com>
2010-05-17 16:07:35 -0500

    added new_password evaluation to account

--- lib/dragons_keep/account.rb
+++ lib/dragons_keep/account.rb
@@ -3,25 +3,32 @@
 require 'activerecord'
 module DragonsKeep
   class Account < ActiveRecord::Base
-      attr_accessor :unencrypted_password, :password_confirmation
-      def encrypt_password(encrypt_pass)
+    attr_accessor :unencrypted_password, :password_confirmation
 
-      end
-      def decrpyt_password(encrypt_pass)
+    def new_password?()
+      return !(self.unencrypted_password.blank? and self.password_confirmation.blank?)
+    end
+    def encrypt_password(encrypt_pass)
+      
+    end
 
-      end
-      def generate_password( length_of_pass, special_char )
-        chars = ("a".."z").to_a
-        chars << ("A".."Z").to_a
-        chars << ("0".."9").to_a
-        if(special_char)
-          chars << ["@", "!", "_",].to_a
-        end
-        newpass = ""
-        1.upto(length_of_pass) { |i| newpass << chars[rand(chars.size-1)] }
-        #self.password
-
-      end
+    def decrpyt_password(encrypt_pass)
 
     end
+
+
+    def generate_password( length_of_pass, special_char )
+      chars = ("a".."z").to_a
+      chars << ("A".."Z").to_a
+      chars << ("0".."9").to_a
+      if(special_char)
+        chars << ["@", "!", "_",].to_a
+      end
+      newpass = ""
+      1.upto(length_of_pass) { |i| newpass << chars[rand(chars.size-1)] }
+      #self.password
+
+    end
+
+  end
 end
